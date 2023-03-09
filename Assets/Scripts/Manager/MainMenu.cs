710473c62ec6dadc89ca79bdd92c71dc5e6c8c73
ClassicSams <samuelreplingerm@gmail.com>
2023-03-09 13:42:13 +0100

    stff

--- Assets/Scripts/Manager/MainMenu.cs
+++ Assets/Scripts/Manager/MainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public string StartScene;
+    public string startScene;
 
 
 
@@ -22,6 +22,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
+        SceneManager.LoadScene(startScene);
 
 
     }
